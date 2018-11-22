6d804f51c00b35df640164eede8a919ecccfc509
AlekseyBilan <bilan.aleksey.sergeevich@gmail.com>
2018-11-22 19:44:04 +0200

    add fixes for app component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
-import React, { Component } from 'react';
-import TickerList from "./Ticker/TickerList";
+import React, {Component} from 'react';
+import currencies from "./currencies";
+import Ticker from "./Ticker/Ticker";
+import './ticker.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <aside>
-            <TickerList/>
-        </aside>
-      </div>
-    );
-  }
-}
+    state = {
+        selectedPairs: []
+    };
 
+    render() {
+        return (
+            <div className="App">
+                <aside>
+                    <ul className="currList">
+                        {this.getHtml(currencies, this.handleChange)}
+                    </ul>
+                </aside>
+                <main>
+                    {
+                        this.state.selectedPairs.map( pair => <Ticker key = {pair} pair = {pair}/>)
+                    }
+                </main>
+            </div>
+        );
+    }
+
+    getHtml(currencies) {
+        return currencies.map((curr, index) => (
+            <li key={index} className='ticker'>
+                <input type="checkbox" id={curr} onChange={this.handleChange(curr)}/>
+                <label htmlFor={curr}>{curr.toUpperCase()}</label>
+            </li>));
+    }
+
+    handleChange = (currency) => event => {
+        const checked = event.target.checked;
+
+        this.setState (//async function
+            ({selectedPairs}) => {
+                let pairs = [...selectedPairs]; //selectedPairs.slice()
+                if(checked && !pairs.includes(currency)){
+                    pairs.push(currency);
+                } else {
+                    pairs = pairs.filter(pair => pair !== currency);
+                }
+                return {selectedPairs: pairs}
+            }
+        )
+    }
+}
 export default App;
